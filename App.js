fb68510e648ae5bf4b3e1aa120f2f680a17ccbdb
Tushar223108 <108059099+Tushar223108@users.noreply.github.com>
2022-07-16 17:28:32 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./App.css";
 import TodoApp from "./component/TodoApp";
+import { newFunction } from "./newFunction";
 
 function App() {
   return (
     <div className="App">
-      <span className="title">TODO LIST</span> <br />
-      <TodoApp />
+      <span className="title">Todo List</span> <br />
+      <div className="addItems">
+      </div>
+          {newFunction()}
+      <div className="col shadow main-col bg-white">
+        <div className="row bg-primary text-white">
+
+        </div>
+        
+      </div>      <TodoApp />
     </div>
   );
 }
 
 export default App;
+
+
